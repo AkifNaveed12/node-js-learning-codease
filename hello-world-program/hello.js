bc6364585b52f3d56636b4f8b1989859c7f6569c
AkifNaveed12 <akif27031@gmail.com>
2026-09-09 14:03:03 +0500

    feat: learnt writing custom scripts, packages  and executed 1st node program

---
+++ hello-world-program/hello.js
@@ -0,0 +1,7 @@
+console.log("hello world! im akif");
+
+
+console.log(window) // gives error here because v8 engines were'nt directly embeded with cpp many ui related stuff like window, document objects were reoved because node is server side
+
+
+
