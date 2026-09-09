f463ab79819df781e93820f982d357beb8898d6e
AkifNaveed12 <akif27031@gmail.com>
2026-09-09 14:33:03 +0500

    feat: learnt and implemented modular code for a math module and learnt require and module.exports, and exports keywords for importing modules

---
+++ modules/index.js
@@ -0,0 +1,9 @@
+const mathModule = require('./mathmodule');
+
+
+console.log(mathModule);
+
+console.log("-------------------------------")
+
+console.log("the sum of the 2 no are :", mathModule.add(1,2));
+console.log("the sub of the 2 no are:", mathModule.sub(4,2));
